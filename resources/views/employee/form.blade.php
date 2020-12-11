933ca25337733975594c10bd6e3335f4608dc23c
tsuji satoru <s-tsuji@clinks.jp>
2020-12-12 01:22:54 +0900

    csrf

--- resources/views/employee/form.blade.php
+++ resources/views/employee/form.blade.php
@@ -1,5 +1,6 @@
 @section('content')
-<form>
+<form method="POST" action="{{route('store')}}">
+    @csrf
     <div class="form-group">
         <label for="exampleInputName">name</label>
         <input type="name" class="form-control" id="exampleInputName" placeholder="name">
@@ -13,8 +14,9 @@
         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
     </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">追加する</button>
 </form>
+<a class="btn btn-secondary" href="{{route(index)}}">戻る</a>
 @endsection
 
 
